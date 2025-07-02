461716950faeaf9e19b20c9e9fc50c7221b508bf
milancodess <milanbhandari153@gmail.com>
2025-07-03 00:44:55 +0545

    Second Useless Commit

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express")
-const milan = require("./second")
 
 console.log("Hello world", milan)
 
--- second.js
+++
@@ -1,9 +0,0 @@
-// function(exports, require, module, __filename, __dirname){
- milan = {
-    name: "Milan",
-    age: 18,
-    developer: true
-}
-
-module.exports = milan
-// }
